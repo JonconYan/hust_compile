c6f11e62665b6c2bf15a5e305c84b9a80c6be655
yxk <1061835743@qq.com>
2020-06-11 21:07:09 +0800

    实验包上传

---
+++ lab2/U201714477-严小康/test2.c
@@ -0,0 +1,8 @@
+int f2(int a,int b)
+{
+    int a,b=0;
+    a+b=10;
+    (a+b)++;
+    ++a++;
+    return 12.3;
+}
---
+++ lab2/U201714477-严小康/test3.c
@@ -0,0 +1,10 @@
+int f3()
+{
+   int a,b;
+   if (a-b) continue;//continue不在循环内
+   else break;//break不在循环内
+   while ( a || f2(1,2)) {while(1) break;continue;}
+   for(a=1;a>0 && f2(1,2)>0;a++)
+ 	if (a+b==0) break;
+   return y;
+}
